237250c375ec41b0adc0c010459d10a8eb39836a
Christopher <leechrs1@gmail.com>
2022-06-16 21:13:36 -0500

    can't seem to create a user in insomnia

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -32,15 +32,15 @@ router.get('/:id', (req, res) => {
 
 // POST /api/users -- create a user/sign up
 router.post('/', (req, res) => {
-    User.create({
-        username: req.body.username,
-        email: req.body.email,
-        password: req.body.password
-    })
+     User.create({
+    username: req.body.username,
+    email: req.body.email,
+    password: req.body.password
+  })
     .then(dbUserData => res.json(dbUserData))
     .catch(err => {
-        console.log(err);
-        res.status(500).json(err);
+      console.log(err);
+      res.status(500).json(err);
     });
 });
 
