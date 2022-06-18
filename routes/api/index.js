5b614e423ffb9a3ebd87a63e8ebe0f3ae8637498
Christopher <leechrs1@gmail.com>
2022-06-17 21:06:11 -0500

    still working on post-routes

--- routes/api/index.js
+++ routes/api/index.js
@@ -4,6 +4,6 @@ const userRoutes = require('./user-routes.js');
 const postRoutes = require('./post-routes')
 
 router.use('/users', userRoutes);
-router.user('/posts', postRoutes);
+router.use('/posts', postRoutes);
 
 module.exports = router;
