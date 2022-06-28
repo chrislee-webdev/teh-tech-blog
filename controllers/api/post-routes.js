3e075eb705508c85e9b54839d958677654a9029b
Christopher <leechrs1@gmail.com>
2022-06-27 21:17:28 -0500

    fixed some small errors but still can't delete a post

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -101,6 +101,7 @@ router.put('/:id', withAuth, (req, res) => {
 });
 
 router.delete('/:id', withAuth, (req, res) => {
+    console.log('id', req.params.id)
     Post.destroy({
         where: {
             id: req.params.id
